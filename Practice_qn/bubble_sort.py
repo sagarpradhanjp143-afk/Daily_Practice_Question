530bf8cd756ebb2e5f1cd6e28a29f7cd9663f587
Sagar Pradhan <sagarpradhanjp143@gmail.com>
2026-08-07 22:57:50 +0530

    Add files via upload

--- Practice_qn/bubble_sort.py
+++ Practice_qn/bubble_sort.py
@@ -1,6 +1,10 @@
-# nums=[1,2,4,5,9,10,12,14,3]
-# n=len(nums)
-# for i in range(n-2,-1,-1):
-#     for j in range(0,i+1):
-#         if 
+nums=[1,6,4,5,9,8,10,12,14,3]
+n=len(nums)
+for i in range(n-2,-1,-1):
+    for j in range(0,i+1):
+        if nums[j]>nums[j+1]:
+            nums[j],nums[j+1]=nums[j+1],nums[j]
+
+print(nums)
+
 
