f607d554a5d77970ae7a5eb8f9e72cd7fc8a61e5
Sagar Pradhan <sagarpradhanjp143@gmail.com>
2026-09-01 21:57:27 +0530

    Add files via upload

---
+++ Practice_qn/leetcode128.py
@@ -0,0 +1,18 @@
+nums=[1,99,101,98,2,5,3,100,1,1]
+n=len(nums)
+my_set=set()
+for i in range(0,n):
+    my_set.add(nums[i])
+
+longest=0
+for num in my_set:
+    if num-1 not in my_set:
+        x=num
+        count=1
+        while x+1 in my_set:
+            count+=1
+            x+=1
+
+        longest=max(longest,count)
+
+print(longest)
