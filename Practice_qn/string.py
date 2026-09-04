954d4dbcfcef77ee7bbf75d957e128ec7a125bc7
Sagar Pradhan <sagarpradhanjp143@gmail.com>
2026-09-04 14:21:44 +0530

    Add files via upload

---
+++ Practice_qn/string.py
@@ -0,0 +1,19 @@
+# word="sagar"
+# word1="pradhan"
+# print(word+" "+word1)
+
+# word="sagar"
+# print(word[0])
+
+# word="Bikash"
+# for ch in word:
+#     print(ch)
+
+# word="Pradhan"
+# print(word[:1])
+
+a=10
+b=11
+# F-string
+s=a+b
+# print(f"avg of   {a} & {b} is {(a+b)/2}")
