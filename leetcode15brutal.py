d860cfe69e421e40e50591787c07160f494d0357
Sagar Pradhan <sagarpradhanjp143@gmail.com>
2026-09-05 10:17:32 +0530

    Add files via upload

---
+++ leetcode15brutal.py
@@ -0,0 +1,11 @@
+     n=len(nums)
+        my_set=set()
+        for i in range(0,n):
+            for j in range(i+1,n):
+                for k in range(j+1,n):
+                    if nums[i]+nums[j]+nums[k]==0:
+                        temp=[nums[i],nums[j],nums[k]]
+                        temp.sort()
+                        my_set.add(tuple(temp))
+
+        return [list(ans) for ans in my_set]
